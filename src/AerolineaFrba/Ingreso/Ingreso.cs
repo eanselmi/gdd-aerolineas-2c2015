58fe990d5e04842144eea48e4a86668e25f46017
guido <guidoberezovsky@hotmail.com>
2015-09-29 22:39:35 -0300

    conexion SQL, pruebe cada uno que le funcione bien

--- src/AerolineaFrba/Ingreso/Ingreso.cs
+++ src/AerolineaFrba/Ingreso/Ingreso.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 using AerolineaFrba;
 
 namespace AerolineaFrba.Ingreso
@@ -24,18 +25,36 @@ namespace AerolineaFrba.Ingreso
            //Validar campos ingresados y buscar en la BD
            //Si valida correctamente ingresa
            //Apertura formulario menu para administrador
-            funcionesComunes.deshabilitarVentanaYAbrirNueva(new menuPrincipal());
-           
+            SqlConnection cn = new SqlConnection("Data Source=(local)" + "\\" + "SQLSERVER2012;Initial Catalog=GD2C2015;User ID=gd;Password=gd2015;");
+            cn.Open();
+            SqlCommand cmd = new SqlCommand("select PASSWORD from AERO.usuarios where USERNAME = '" + this.textUsuario.Text + "'",cn);
+            SqlDataReader dr;
+            dr = cmd.ExecuteReader();
+            String contr = "";
+            if (dr.HasRows) {
+                while (dr.Read()){
+                    contr = dr.GetString(0);
+                }
+                Base b = new Base();
+                if (contr == b.pasarASha256(this.textPassword.Text)){
+                    //TODO: update intento exitoso procedure
+                    funcionesComunes.deshabilitarVentanaYAbrirNueva(new menuPrincipal());
+                }else{
+                    //TODO: update intento fallido procedure
+                    MessageBox.Show("Contraseña inválida, tenga en cuenta que son 3 intentos en total");
+                }
+            }else{
+                MessageBox.Show("Usuario inválido");
+            }
+            this.textUsuario.Clear();
+            this.textPassword.Clear();
+            dr.Close();
         }
 
-        private void botonInvitado_Click(object sender, EventArgs e)
-        {
+        private void botonInvitado_Click(object sender, EventArgs e) {
             //Apertura formulario menu para invitado
             funcionesComunes.deshabilitarVentanaYAbrirNueva(new menuPrincipal());
-            
-          
         }
-
 
         //este boton borra el contenido de los input
         private void botonLimpiar_Click(object sender, EventArgs e)
@@ -43,7 +62,5 @@ namespace AerolineaFrba.Ingreso
             this.textUsuario.Clear();
             this.textPassword.Clear();
         }
-
- 
     }
 }
