80e02834988e6867801c655d741569b4becbd8b0
AdrianoAlianiello <adriano.alianiello@gmail.com>
2015-10-04 13:37:33 -0300

    Filtros en tablas

--- src/AerolineaFrba/Registro de Usuario/bajaModificacionDeCliente.Designer.cs
+++ src/AerolineaFrba/Registro de Usuario/bajaModificacionDeCliente.Designer.cs
@@ -34,10 +34,17 @@
             this.botonVolver = new System.Windows.Forms.Button();
             this.botonBaja = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textNombre = new System.Windows.Forms.TextBox();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.textApellido = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBoxFiltro = new System.Windows.Forms.ComboBox();
+            this.textDni = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBoxBusqueda = new System.Windows.Forms.TextBox();
+            this.textMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textTelefono = new System.Windows.Forms.TextBox();
+            this.botonBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridListadoClientes)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -52,6 +59,7 @@
             this.botonLimpiar.TabIndex = 23;
             this.botonLimpiar.Text = "Limpiar";
             this.botonLimpiar.UseVisualStyleBackColor = true;
+            this.botonLimpiar.Click += new System.EventHandler(this.botonLimpiar_Click);
             // 
             // dataGridListadoClientes
             // 
@@ -59,12 +67,12 @@
             this.dataGridListadoClientes.AllowUserToDeleteRows = false;
             this.dataGridListadoClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridListadoClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridListadoClientes.Location = new System.Drawing.Point(12, 99);
+            this.dataGridListadoClientes.Location = new System.Drawing.Point(12, 170);
             this.dataGridListadoClientes.Name = "dataGridListadoClientes";
             this.dataGridListadoClientes.ReadOnly = true;
             this.dataGridListadoClientes.RowHeadersVisible = false;
             this.dataGridListadoClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridListadoClientes.Size = new System.Drawing.Size(698, 297);
+            this.dataGridListadoClientes.Size = new System.Drawing.Size(698, 248);
             this.dataGridListadoClientes.TabIndex = 22;
             // 
             // botonModificacion
@@ -103,66 +111,126 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboBoxFiltro);
+            this.groupBox1.Controls.Add(this.botonBuscar);
+            this.groupBox1.Controls.Add(this.textTelefono);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textMail);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textDni);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBoxBusqueda);
+            this.groupBox1.Controls.Add(this.textApellido);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textNombre);
+            this.groupBox1.Controls.Add(this.lblApellido);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(114, 3);
+            this.groupBox1.Location = new System.Drawing.Point(114, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(494, 89);
-            this.groupBox1.TabIndex = 19;
+            this.groupBox1.Size = new System.Drawing.Size(501, 159);
+            this.groupBox1.TabIndex = 24;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtros";
             // 
+            // textNombre
+            // 
+            this.textNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNombre.Location = new System.Drawing.Point(9, 34);
+            this.textNombre.Name = "textNombre";
+            this.textNombre.Size = new System.Drawing.Size(148, 26);
+            this.textNombre.TabIndex = 9;
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(6, 18);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(57, 16);
+            this.lblApellido.TabIndex = 6;
+            this.lblApellido.Text = "Nombre";
+            // 
+            // textApellido
+            // 
+            this.textApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textApellido.Location = new System.Drawing.Point(9, 79);
+            this.textApellido.Name = "textApellido";
+            this.textApellido.Size = new System.Drawing.Size(148, 26);
+            this.textApellido.TabIndex = 11;
+            // 
             // label1
             // 
-            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 20);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 63);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 20);
-            this.label1.TabIndex = 55;
-            this.label1.Text = "Búsqueda";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Apellido";
             // 
-            // comboBoxFiltro
+            // textDni
             // 
-            this.comboBoxFiltro.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBoxFiltro.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBoxFiltro.Items.AddRange(new object[] {
-            "Nombre",
-            "Apellido",
-            "Dni",
-            "Dirección",
-            "Teléfono",
-            "Mail",
-            "Fecha de Nacimiento"});
-            this.comboBoxFiltro.Location = new System.Drawing.Point(258, 42);
-            this.comboBoxFiltro.Name = "comboBoxFiltro";
-            this.comboBoxFiltro.Size = new System.Drawing.Size(227, 28);
-            this.comboBoxFiltro.TabIndex = 54;
-            this.comboBoxFiltro.Tag = "";
+            this.textDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDni.Location = new System.Drawing.Point(187, 34);
+            this.textDni.Name = "textDni";
+            this.textDni.Size = new System.Drawing.Size(118, 26);
+            this.textDni.TabIndex = 13;
             // 
             // label2
             // 
-            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(254, 18);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(184, 18);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 20);
-            this.label2.TabIndex = 53;
-            this.label2.Text = "Filtro de Búsqueda";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "DNI";
             // 
-            // textBoxBusqueda
+            // textMail
             // 
-            this.textBoxBusqueda.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxBusqueda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxBusqueda.Location = new System.Drawing.Point(13, 43);
-            this.textBoxBusqueda.Name = "textBoxBusqueda";
-            this.textBoxBusqueda.Size = new System.Drawing.Size(239, 26);
-            this.textBoxBusqueda.TabIndex = 52;
+            this.textMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMail.Location = new System.Drawing.Point(9, 124);
+            this.textMail.Name = "textMail";
+            this.textMail.Size = new System.Drawing.Size(148, 26);
+            this.textMail.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(184, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Teléfono";
+            // 
+            // textTelefono
+            // 
+            this.textTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTelefono.Location = new System.Drawing.Point(187, 79);
+            this.textTelefono.Name = "textTelefono";
+            this.textTelefono.Size = new System.Drawing.Size(118, 26);
+            this.textTelefono.TabIndex = 17;
+            // 
+            // botonBuscar
+            // 
+            this.botonBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonBuscar.Location = new System.Drawing.Point(378, 104);
+            this.botonBuscar.Name = "botonBuscar";
+            this.botonBuscar.Size = new System.Drawing.Size(117, 46);
+            this.botonBuscar.TabIndex = 19;
+            this.botonBuscar.Text = "Buscar";
+            this.botonBuscar.UseVisualStyleBackColor = true;
+            this.botonBuscar.Click += new System.EventHandler(this.botonBuscar_Click);
             // 
             // bajaModificacionDeCliente
             // 
@@ -170,12 +238,12 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(722, 481);
             this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.botonLimpiar);
             this.Controls.Add(this.dataGridListadoClientes);
             this.Controls.Add(this.botonModificacion);
             this.Controls.Add(this.botonVolver);
             this.Controls.Add(this.botonBaja);
-            this.Controls.Add(this.groupBox1);
             this.Name = "bajaModificacionDeCliente";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Listado de Clientes";
@@ -195,9 +263,16 @@
         private System.Windows.Forms.Button botonVolver;
         private System.Windows.Forms.Button botonBaja;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBoxFiltro;
+        private System.Windows.Forms.TextBox textTelefono;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textDni;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBoxBusqueda;
+        private System.Windows.Forms.TextBox textApellido;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textNombre;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.Button botonBuscar;
     }
 }
--- src/AerolineaFrba/Registro de Usuario/bajaModificacionDeCliente.cs
+++ src/AerolineaFrba/Registro de Usuario/bajaModificacionDeCliente.cs
@@ -25,9 +25,63 @@ namespace AerolineaFrba.Registro_de_Usuario
 
         private void bajaModificacionDeCliente_Load(object sender, EventArgs e)
         {
+            consultarClientes();
+        }
+
+        private void botonBuscar_Click(object sender, EventArgs e)
+        {
+            DataTable clientesFiltro = filtrarCliente(textNombre.Text, textApellido.Text, textMail.Text, textDni.Text, textTelefono.Text);
+            dataGridListadoClientes.DataSource = clientesFiltro;
+        }
+
+        private DataTable filtrarCliente(string nombre, string apellido, string mail, string dni, string telefono)
+        {
+            DataTable tablaClientes = listado;
+            var final_rol = "";
+            var posFiltro = true;
+            var filtrosBusqueda = new List<string>();
+            if (nombre != "") filtrosBusqueda.Add("Nombre LIKE '%" + nombre + "%'");
+            if (apellido != "") filtrosBusqueda.Add("Apellido LIKE '%" + apellido + "%'");
+            if (mail != "") filtrosBusqueda.Add("Mail LIKE '%" + mail + "%'");
+            if (dni != "") filtrosBusqueda.Add("Dni = " + dni);
+            if (telefono != "") filtrosBusqueda.Add("Teléfono = " + telefono);
+   
+            foreach (var filtro in filtrosBusqueda)
+            {
+                if (!posFiltro)
+                    final_rol += " AND " + filtro;
+                else
+                {
+                    final_rol += filtro;
+                    posFiltro = false;
+                }
+            }
+            if (tablaClientes != null)
+                tablaClientes.DefaultView.RowFilter = final_rol;
+            return tablaClientes;
+        }
+
+        private void botonLimpiar_Click(object sender, EventArgs e)
+        {
+            limpiar();
+
+        }
+
+        private void limpiar()
+        {
+            dataGridListadoClientes.DataSource = null;
+            consultarClientes();
+            textNombre.Text = "";
+            textApellido.Text = "";
+            textMail.Text = "";
+            textDni.Text = "";
+            textTelefono.Text = "";
+        }
+
+        private void consultarClientes()
+        {
             listado = SqlConnector.obtenerTablaSegunConsultaString("select NOMBRE as Nombre, APELLIDO as Apellido, DNI as Dni, DIRECCION as Dirección, TELEFONO as Teléfono, MAIL as Mail, FECHA_NACIMIENTO as 'Fecha de Nacimiento' from AERO.clientes ");
             dataGridListadoClientes.DataSource = listado;
         }
-
     }
 }
