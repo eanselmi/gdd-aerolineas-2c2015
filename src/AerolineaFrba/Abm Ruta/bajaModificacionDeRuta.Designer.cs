62ec8d174c278eefaee1df5c75cf87a00360ba58
GuidoMarsico <marsico.guido@gmail.com>
2015-10-11 15:34:21 -0300

    En la vista de bajaModificacionRuta hago que el listado tome las rutas desde la base

--- src/AerolineaFrba/Abm Ruta/bajaModificacionDeRuta.Designer.cs
+++ src/AerolineaFrba/Abm Ruta/bajaModificacionDeRuta.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.botonLimpiar = new System.Windows.Forms.Button();
-            this.dataGridListadoAeronaves = new System.Windows.Forms.DataGridView();
+            this.dataGridListadoRutas = new System.Windows.Forms.DataGridView();
             this.botonModificacion = new System.Windows.Forms.Button();
             this.botonVolver = new System.Windows.Forms.Button();
             this.botonBaja = new System.Windows.Forms.Button();
@@ -38,7 +38,7 @@
             this.comboBoxFiltro = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBoxBusqueda = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridListadoAeronaves)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridListadoRutas)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,28 +46,29 @@
             // 
             this.botonLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonLimpiar.Location = new System.Drawing.Point(421, 428);
+            this.botonLimpiar.Location = new System.Drawing.Point(532, 428);
             this.botonLimpiar.Name = "botonLimpiar";
-            this.botonLimpiar.Size = new System.Drawing.Size(117, 46);
+            this.botonLimpiar.Size = new System.Drawing.Size(135, 46);
             this.botonLimpiar.TabIndex = 17;
             this.botonLimpiar.Text = "Limpiar";
             this.botonLimpiar.UseVisualStyleBackColor = true;
             // 
-            // dataGridListadoAeronaves
+            // dataGridListadoRutas
             // 
-            this.dataGridListadoAeronaves.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridListadoAeronaves.Location = new System.Drawing.Point(45, 111);
-            this.dataGridListadoAeronaves.Name = "dataGridListadoAeronaves";
-            this.dataGridListadoAeronaves.Size = new System.Drawing.Size(493, 297);
-            this.dataGridListadoAeronaves.TabIndex = 16;
+            this.dataGridListadoRutas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridListadoRutas.Location = new System.Drawing.Point(45, 111);
+            this.dataGridListadoRutas.Name = "dataGridListadoRutas";
+            this.dataGridListadoRutas.RowHeadersVisible = false;
+            this.dataGridListadoRutas.Size = new System.Drawing.Size(622, 297);
+            this.dataGridListadoRutas.TabIndex = 16;
             // 
             // botonModificacion
             // 
             this.botonModificacion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonModificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonModificacion.Location = new System.Drawing.Point(298, 428);
+            this.botonModificacion.Location = new System.Drawing.Point(372, 428);
             this.botonModificacion.Name = "botonModificacion";
-            this.botonModificacion.Size = new System.Drawing.Size(117, 46);
+            this.botonModificacion.Size = new System.Drawing.Size(139, 46);
             this.botonModificacion.TabIndex = 15;
             this.botonModificacion.Text = "Modificación";
             this.botonModificacion.UseVisualStyleBackColor = true;
@@ -77,9 +78,9 @@
             // 
             this.botonVolver.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonVolver.Location = new System.Drawing.Point(175, 428);
+            this.botonVolver.Location = new System.Drawing.Point(202, 428);
             this.botonVolver.Name = "botonVolver";
-            this.botonVolver.Size = new System.Drawing.Size(117, 46);
+            this.botonVolver.Size = new System.Drawing.Size(139, 46);
             this.botonVolver.TabIndex = 14;
             this.botonVolver.Text = "Volver";
             this.botonVolver.UseVisualStyleBackColor = true;
@@ -89,9 +90,9 @@
             // 
             this.botonBaja.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonBaja.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonBaja.Location = new System.Drawing.Point(52, 428);
+            this.botonBaja.Location = new System.Drawing.Point(44, 428);
             this.botonBaja.Name = "botonBaja";
-            this.botonBaja.Size = new System.Drawing.Size(117, 46);
+            this.botonBaja.Size = new System.Drawing.Size(127, 46);
             this.botonBaja.TabIndex = 12;
             this.botonBaja.Text = "Baja";
             this.botonBaja.UseVisualStyleBackColor = true;
@@ -105,7 +106,7 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(44, 15);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(494, 89);
+            this.groupBox1.Size = new System.Drawing.Size(623, 89);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filtros";
@@ -130,9 +131,9 @@
             "Origen",
             "Destino",
             "Servicios"});
-            this.comboBoxFiltro.Location = new System.Drawing.Point(258, 42);
+            this.comboBoxFiltro.Location = new System.Drawing.Point(342, 41);
             this.comboBoxFiltro.Name = "comboBoxFiltro";
-            this.comboBoxFiltro.Size = new System.Drawing.Size(227, 28);
+            this.comboBoxFiltro.Size = new System.Drawing.Size(275, 28);
             this.comboBoxFiltro.TabIndex = 54;
             // 
             // label2
@@ -140,7 +141,7 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(254, 18);
+            this.label2.Location = new System.Drawing.Point(338, 18);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(143, 20);
             this.label2.TabIndex = 53;
@@ -153,17 +154,17 @@
             this.textBoxBusqueda.Location = new System.Drawing.Point(13, 43);
             this.textBoxBusqueda.Name = "textBoxBusqueda";
             this.textBoxBusqueda.PasswordChar = '*';
-            this.textBoxBusqueda.Size = new System.Drawing.Size(239, 26);
+            this.textBoxBusqueda.Size = new System.Drawing.Size(306, 26);
             this.textBoxBusqueda.TabIndex = 52;
             // 
             // bajaModificacionDeRuta
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(582, 488);
+            this.ClientSize = new System.Drawing.Size(718, 488);
             this.ControlBox = false;
             this.Controls.Add(this.botonLimpiar);
-            this.Controls.Add(this.dataGridListadoAeronaves);
+            this.Controls.Add(this.dataGridListadoRutas);
             this.Controls.Add(this.botonModificacion);
             this.Controls.Add(this.botonVolver);
             this.Controls.Add(this.botonBaja);
@@ -171,7 +172,8 @@
             this.Name = "bajaModificacionDeRuta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Listado de Rutas";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridListadoAeronaves)).EndInit();
+            this.Load += new System.EventHandler(this.bajaModificacionDeRuta_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridListadoRutas)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -181,7 +183,7 @@
         #endregion
 
         private System.Windows.Forms.Button botonLimpiar;
-        private System.Windows.Forms.DataGridView dataGridListadoAeronaves;
+        private System.Windows.Forms.DataGridView dataGridListadoRutas;
         private System.Windows.Forms.Button botonModificacion;
         private System.Windows.Forms.Button botonVolver;
         private System.Windows.Forms.Button botonBaja;
--- src/AerolineaFrba/Abm Ruta/bajaModificacionDeRuta.cs
+++ src/AerolineaFrba/Abm Ruta/bajaModificacionDeRuta.cs
@@ -13,6 +13,7 @@ namespace AerolineaFrba.Abm_Ruta
 {
     public partial class bajaModificacionDeRuta : Form
     {
+        DataTable listado;
         public bajaModificacionDeRuta()
         {
             InitializeComponent();
@@ -26,6 +27,23 @@ namespace AerolineaFrba.Abm_Ruta
         private void botonVolver_Click(object sender, EventArgs e)
         {
             funcionesComunes.habilitarAnterior();
+        }
+
+        private void bajaModificacionDeRuta_Load(object sender, EventArgs e)
+        {
+            consultarRutas();
+        }
+
+        private void consultarRutas()
+        {
+            listado = SqlConnector.obtenerTablaSegunConsultaString(@"SELECT r.ID as ID,r.CODIGO as Codigo,
+                r.PRECIO_BASE_KG as 'Precio Base Kg',
+                r.PRECIO_BASE_PASAJE as 'Precio Base Pasaje',c1.NOMBRE as Origen,
+                c2.NOMBRE as Destino,t.NOMBRE as 'Tipo de Servicio' 
+                from AERO.rutas r, AERO.aeropuertos c1,AERO.aeropuertos c2,AERO.tipos_de_servicio t  
+                WHERE  r.ORIGEN_ID = c1.ID AND r.DESTINO_ID=c2.ID AND r.TIPO_SERVICIO_ID = t.ID ");
+            dataGridListadoRutas.DataSource = listado;
+            dataGridListadoRutas.Columns[0].Visible = false;
         }
     }
 }
