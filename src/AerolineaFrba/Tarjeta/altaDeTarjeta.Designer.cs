bf0bc9ad2e8696b7f13cda344d8e844f56fe84a6
GuidoMarsico <marsico.guido@gmail.com>
2015-10-18 01:21:39 -0300

    Valido datos en el alta de la tarjeta , falta realizar el insert

--- src/AerolineaFrba/Tarjeta/altaDeTarjeta.Designer.cs
+++ src/AerolineaFrba/Tarjeta/altaDeTarjeta.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label8 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -37,32 +36,24 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBoxNumero = new System.Windows.Forms.TextBox();
             this.label13 = new System.Windows.Forms.Label();
-            this.timePickerVencimiento = new System.Windows.Forms.DateTimePicker();
             this.label9 = new System.Windows.Forms.Label();
             this.botonCancelar = new System.Windows.Forms.Button();
             this.botonLimpiar = new System.Windows.Forms.Button();
             this.botonGuardar = new System.Windows.Forms.Button();
             this.label14 = new System.Windows.Forms.Label();
             this.textBoxDni = new System.Windows.Forms.TextBox();
+            this.fechavencimiento = new System.Windows.Forms.DateTimePicker();
+            this.dataGridCliente = new System.Windows.Forms.DataGridView();
+            this.botonBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCliente)).BeginInit();
             this.SuspendLayout();
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.Red;
-            this.label8.Location = new System.Drawing.Point(340, 32);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(15, 20);
-            this.label8.TabIndex = 20;
-            this.label8.Text = "*";
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(72, 9);
+            this.label1.Location = new System.Drawing.Point(66, 31);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 20);
             this.label1.TabIndex = 19;
@@ -73,7 +64,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(340, 81);
+            this.label2.Location = new System.Drawing.Point(287, 290);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(15, 20);
             this.label2.TabIndex = 23;
@@ -84,7 +75,7 @@
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(72, 61);
+            this.label3.Location = new System.Drawing.Point(352, 264);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(39, 20);
             this.label3.TabIndex = 22;
@@ -94,12 +85,7 @@
             // 
             this.comboBoxTipoTarjeta.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBoxTipoTarjeta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBoxTipoTarjeta.Items.AddRange(new object[] {
-            "American Express",
-            "Mastercard",
-            "Visa",
-            "Otros"});
-            this.comboBoxTipoTarjeta.Location = new System.Drawing.Point(76, 81);
+            this.comboBoxTipoTarjeta.Location = new System.Drawing.Point(346, 290);
             this.comboBoxTipoTarjeta.Name = "comboBoxTipoTarjeta";
             this.comboBoxTipoTarjeta.Size = new System.Drawing.Size(261, 28);
             this.comboBoxTipoTarjeta.TabIndex = 24;
@@ -109,7 +95,7 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(340, 135);
+            this.label4.Location = new System.Drawing.Point(613, 287);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(15, 20);
             this.label4.TabIndex = 27;
@@ -120,7 +106,7 @@
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(72, 112);
+            this.label5.Location = new System.Drawing.Point(19, 267);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(65, 20);
             this.label5.TabIndex = 26;
@@ -130,39 +116,30 @@
             // 
             this.textBoxNumero.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNumero.Location = new System.Drawing.Point(76, 135);
+            this.textBoxNumero.Location = new System.Drawing.Point(23, 290);
             this.textBoxNumero.MaxLength = 16;
             this.textBoxNumero.Name = "textBoxNumero";
-            this.textBoxNumero.Size = new System.Drawing.Size(261, 26);
+            this.textBoxNumero.Size = new System.Drawing.Size(258, 26);
             this.textBoxNumero.TabIndex = 25;
+            this.textBoxNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.soloNumer);
             // 
             // label13
             // 
             this.label13.AutoSize = true;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label13.ForeColor = System.Drawing.Color.Red;
-            this.label13.Location = new System.Drawing.Point(340, 187);
+            this.label13.Location = new System.Drawing.Point(287, 352);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(15, 20);
             this.label13.TabIndex = 33;
             this.label13.Text = "*";
-            // 
-            // timePickerVencimiento
-            // 
-            this.timePickerVencimiento.CustomFormat = "MM/yy";
-            this.timePickerVencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.timePickerVencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.timePickerVencimiento.Location = new System.Drawing.Point(76, 187);
-            this.timePickerVencimiento.Name = "timePickerVencimiento";
-            this.timePickerVencimiento.Size = new System.Drawing.Size(258, 26);
-            this.timePickerVencimiento.TabIndex = 32;
             // 
             // label9
             // 
             this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(72, 164);
+            this.label9.Location = new System.Drawing.Point(19, 319);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(146, 20);
             this.label9.TabIndex = 31;
@@ -172,9 +149,9 @@
             // 
             this.botonCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonCancelar.Location = new System.Drawing.Point(269, 333);
+            this.botonCancelar.Location = new System.Drawing.Point(456, 419);
             this.botonCancelar.Name = "botonCancelar";
-            this.botonCancelar.Size = new System.Drawing.Size(105, 42);
+            this.botonCancelar.Size = new System.Drawing.Size(163, 42);
             this.botonCancelar.TabIndex = 37;
             this.botonCancelar.Text = "Cancelar";
             this.botonCancelar.UseVisualStyleBackColor = true;
@@ -184,9 +161,9 @@
             // 
             this.botonLimpiar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonLimpiar.Location = new System.Drawing.Point(161, 333);
+            this.botonLimpiar.Location = new System.Drawing.Point(261, 419);
             this.botonLimpiar.Name = "botonLimpiar";
-            this.botonLimpiar.Size = new System.Drawing.Size(105, 42);
+            this.botonLimpiar.Size = new System.Drawing.Size(130, 42);
             this.botonLimpiar.TabIndex = 36;
             this.botonLimpiar.Text = "Limpiar";
             this.botonLimpiar.UseVisualStyleBackColor = true;
@@ -196,9 +173,9 @@
             // 
             this.botonGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.botonGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.botonGuardar.Location = new System.Drawing.Point(53, 333);
+            this.botonGuardar.Location = new System.Drawing.Point(23, 419);
             this.botonGuardar.Name = "botonGuardar";
-            this.botonGuardar.Size = new System.Drawing.Size(105, 42);
+            this.botonGuardar.Size = new System.Drawing.Size(141, 42);
             this.botonGuardar.TabIndex = 35;
             this.botonGuardar.Text = "Guardar";
             this.botonGuardar.UseVisualStyleBackColor = true;
@@ -209,7 +186,7 @@
             this.label14.AutoSize = true;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label14.ForeColor = System.Drawing.Color.Red;
-            this.label14.Location = new System.Drawing.Point(194, 216);
+            this.label14.Location = new System.Drawing.Point(453, 333);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(140, 16);
             this.label14.TabIndex = 34;
@@ -220,24 +197,64 @@
             this.textBoxDni.AcceptsReturn = true;
             this.textBoxDni.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxDni.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxDni.Location = new System.Drawing.Point(76, 32);
+            this.textBoxDni.Location = new System.Drawing.Point(109, 25);
             this.textBoxDni.MaxLength = 8;
             this.textBoxDni.Name = "textBoxDni";
-            this.textBoxDni.Size = new System.Drawing.Size(261, 26);
+            this.textBoxDni.Size = new System.Drawing.Size(142, 26);
             this.textBoxDni.TabIndex = 18;
+            this.textBoxDni.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.soloNumer);
+            // 
+            // fechavencimiento
+            // 
+            this.fechavencimiento.CustomFormat = "dd/MM/yyyy ";
+            this.fechavencimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fechavencimiento.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.fechavencimiento.Location = new System.Drawing.Point(23, 352);
+            this.fechavencimiento.Name = "fechavencimiento";
+            this.fechavencimiento.Size = new System.Drawing.Size(258, 26);
+            this.fechavencimiento.TabIndex = 65;
+            // 
+            // dataGridCliente
+            // 
+            this.dataGridCliente.AllowUserToAddRows = false;
+            this.dataGridCliente.AllowUserToDeleteRows = false;
+            this.dataGridCliente.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridCliente.Location = new System.Drawing.Point(23, 57);
+            this.dataGridCliente.MultiSelect = false;
+            this.dataGridCliente.Name = "dataGridCliente";
+            this.dataGridCliente.ReadOnly = true;
+            this.dataGridCliente.RowHeadersVisible = false;
+            this.dataGridCliente.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridCliente.Size = new System.Drawing.Size(596, 181);
+            this.dataGridCliente.TabIndex = 66;
+            // 
+            // botonBuscar
+            // 
+            this.botonBuscar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.botonBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonBuscar.Location = new System.Drawing.Point(356, 19);
+            this.botonBuscar.Name = "botonBuscar";
+            this.botonBuscar.Size = new System.Drawing.Size(113, 32);
+            this.botonBuscar.TabIndex = 67;
+            this.botonBuscar.Text = "Buscar";
+            this.botonBuscar.UseVisualStyleBackColor = true;
+            this.botonBuscar.Click += new System.EventHandler(this.botonBuscar_Click);
             // 
             // altaDeTarjeta
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(432, 387);
+            this.ClientSize = new System.Drawing.Size(640, 473);
             this.ControlBox = false;
+            this.Controls.Add(this.botonBuscar);
+            this.Controls.Add(this.dataGridCliente);
+            this.Controls.Add(this.fechavencimiento);
             this.Controls.Add(this.botonCancelar);
             this.Controls.Add(this.botonLimpiar);
             this.Controls.Add(this.botonGuardar);
             this.Controls.Add(this.label14);
             this.Controls.Add(this.label13);
-            this.Controls.Add(this.timePickerVencimiento);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label5);
@@ -245,12 +262,12 @@
             this.Controls.Add(this.comboBoxTipoTarjeta);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxDni);
             this.Name = "altaDeTarjeta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Nueva Tarjeta";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridCliente)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -258,7 +275,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -267,12 +283,14 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBoxNumero;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.DateTimePicker timePickerVencimiento;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button botonCancelar;
         private System.Windows.Forms.Button botonLimpiar;
         private System.Windows.Forms.Button botonGuardar;
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.TextBox textBoxDni;
+        private System.Windows.Forms.DateTimePicker fechavencimiento;
+        private System.Windows.Forms.DataGridView dataGridCliente;
+        private System.Windows.Forms.Button botonBuscar;
     }
 }
--- src/AerolineaFrba/Tarjeta/altaDeTarjeta.cs
+++ src/AerolineaFrba/Tarjeta/altaDeTarjeta.cs
@@ -16,6 +16,7 @@ namespace AerolineaFrba.Tarjeta
         public altaDeTarjeta()
         {
             InitializeComponent();
+            funcionesComunes.llenarCombobox(this.comboBoxTipoTarjeta, "NOMBRE", "select ID, NOMBRE from AERO.tipos_tarjeta");
         }
 
         private void botonCancelar_Click(object sender, EventArgs e)
@@ -29,18 +30,65 @@ namespace AerolineaFrba.Tarjeta
             this.textBoxDni.Clear();
             this.textBoxNumero.Clear();
             this.comboBoxTipoTarjeta.SelectedIndex = -1;
-            this.timePickerVencimiento.ResetText();
+            this.dataGridCliente.DataSource = null;
+            this.fechavencimiento.ResetText();
         }
 
         private void botonGuardar_Click(object sender, EventArgs e)
         {
+            if (validarDatos()) {
+                
+                
+            }
+        }
+
+        private void consultarContactos()
+        {
             String dni = this.textBoxDni.Text;
-            String numero = this.textBoxNumero.Text;
-            String func = (String) this.comboBoxTipoTarjeta.SelectedItem;
-            if (dni != "" && numero != "" && func != null)
-                this.textBoxNumero.Clear();
-            else MessageBox.Show("Complete los campos requeridos", "Error", MessageBoxButtons.OK, 
+            if (dni != "")
+            {
+                List<string> lista = new List<string>();
+                lista.Add("@dni");
+                DataTable resultado = SqlConnector.obtenerTablaSegunProcedure("AERO.obtenerClienteConMillas", lista, dni);
+                dataGridCliente.DataSource = resultado;
+                dataGridCliente.Columns[0].Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un numero de documento", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private bool validarDatos()
+        {
+            Int32 numero;
+            if (this.textBoxNumero.Text == "")
+                numero = 0;
+            else
+                numero = Int32.Parse(this.textBoxNumero.Text);
+            if (this.dataGridCliente.DataSource != null && numero > 0 && this.comboBoxTipoTarjeta.SelectedValue != null)
+                return true;
+            else MessageBox.Show("Complete los campos requeridos correctamente", "Error", MessageBoxButtons.OK,
                 MessageBoxIcon.Error);
+            if (this.fechavencimiento.Value > DateTime.Today)
+                return true;
+            else
+                MessageBox.Show("La fecha de vencimiento debe ser superior a la de hoy", "Error", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            return false;
+   
+        }
+
+        private void soloNumer(object sender, KeyPressEventArgs e)
+        {
+            funcionesComunes.soloNumeros(e);
+        }
+
+        private void botonBuscar_Click(object sender, EventArgs e)
+        {
+            consultarContactos();
         }
     }
 }
